3ccfe50093f4b3c8c04a1aa9012e7c42a237b562
Murray Neuzerling <murray@neuzerling.com>
2018-06-12 21:02:37 +1000

    Added the search_emails function

---
+++ R/clean_emails.R
@@ -0,0 +1,41 @@
+#' Clean and augment a list of emails
+#' 
+#' Returns a tibble in which the first column is the given list of emails (as
+#' objects), and the other columns are common attributes of emails.
+#' 
+#' @param emails A list of DCOM email objects.
+#' @keywords
+
+clean_emails <- function(emails) {
+    
+    get_attachment_names <- function(email) {
+        number_attachments = email$Attachments()$Count()
+        if (number_attachments == 0) {
+            return("")
+        }
+        attachments <- purrr::map(
+            seq(number_attachments), 
+            function(x) email$Attachments(x)$FileName()
+        )
+        return(paste(attachments, sep = ", "))
+    }
+    
+    augmented_emails <- tibble::tibble("email" = emails)
+    
+    augmented_emails <- dplyr::mutate(dplyr::rowwise(augmented_emails),
+        subject = email$Subject(),
+        received = as.POSIXct(email$ReceivedTime() * (24 * 60 * 60), 
+                              origin="1899-12-30", tz="GMT"),
+        from_name = email$SenderName(),
+        from_email = email$SenderEmailAddress(),
+        to_name = email$ReceivedByName(),
+        cc_name = email$CC(),
+        importance = email$Importance(),
+        number_attachments = email$Attachments()$Count(),
+        attachments = get_attachment_names(email),
+        body = email$Body()
+    )
+    
+    return(augmented_results)
+
+}
---
+++ R/search_emails.R
@@ -0,0 +1,79 @@
+#' Search for emails
+#'
+#' Searches for all emails satisfying a search term. Searches can be performed
+#' in one of several email attributes, such as subject or sender name. Searches
+#' are always case sensitive. The DCOM search method used is asynchronous, in 
+#' that R will not wait for the search to complete before continuing. We pause
+#' R for a given amount of time (default is 10 seconds) to allow the search to
+#' complete. See Details for more information.
+#' 
+#' There is a package for handling the AdvancedSearchComplete, RDCOMEvents, but
+#' it is not easily available. The proper way to implement this search would be
+#' to wait for the event to return that the search is complete, and then 
+#' continue.
+#' 
+#' @param search_term A case-sensitive search query.
+#' @param folder A folder in which to search.
+#' Defaults to "Inbox".
+#' @param scope The scope of the search: one of "subject", "body", 
+#' "attachment_names", "from_name", "from_email", "cc_name", or "to_name".
+#' @param partial_match If set to TRUE, will allow any number of characters to
+#' appear either side of the search term.
+#' Defaults to TRUE.
+#' @param search_subfolders If set to TRUE, will search subfolders below the
+#' specified `folder`.
+#' Defaults to TRUE.
+#' @param search_time The amount of time, in seconds, to wait for the search to
+#' complete.
+#' @keywords
+#' @export
+
+search_emails <- function(
+    search_term, 
+    folder = "Inbox",
+    scope = "subject", 
+    partial_match = TRUE, 
+    search_subfolders = TRUE,
+    search_time = 10 #seconds
+    ) {
+
+    outlook_app <- COMCreate("Outlook.Application")
+    
+    scope <- if (scope == "subject") {"subject"} 
+    else if (scope == "body") {"textdescription"} 
+    else if (scope == "attachment_names") {"attachmentfilename"}
+    else if (scope == "from_name") {"fromname"} 
+    else if (scope == "from_email") {"fromemail"} 
+    else if (scope == "cc_name") {"displaycc"} 
+    else if (scope == "to_name") {"displayto"} 
+    else {
+        stop(paste0(
+            "Unknown scope for search: ", scope, ". ",
+            "Use one of subject, body, attachment_names, from_name, ",
+            "from_email, cc_name, or to_name."
+        ))
+    }
+    
+    search_query <- paste0(
+        "urn:schemas:httpmail:",
+        scope,
+        if (partial_match) {" LIKE '%"} else {" = '"},
+        search_term,
+        if (partial_match) {"%'"} else {"'"}
+    )
+
+    search <- outlook_app$AdvancedSearch(
+        folder,
+        search_query,
+        search_subfolders
+    )
+    
+    Sys.sleep(search_time)
+    
+    results <- search$results()
+    number_results <- results$Count()
+    emails <- purrr::map(seq(number_results), function(x) results$Item(x))
+    
+    clean_emails(emails)
+
+}
