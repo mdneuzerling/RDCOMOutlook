71ca18200130ac6d00edfd7a51a3bfd705dc6423
Murray Neuzerling <murray@neuzerling.com>
2018-06-14 20:18:57 +1000

    Split code into new function: image_format

--- R/embed_image_cid.R
+++ R/embed_image_cid.R
@@ -2,7 +2,10 @@
 #'
 #' Generate a HTML tag that uses a CID source to display an image. This is used
 #' in emails for images, in which an attached image can be embedded into the
-#' email body.
+#' email body. If the image is a png, jpg or bmp, the file will be scaled down
+#' to the provided `max_height` and `max_width`, preserving the aspect ratio.
+#' If the file is a gif, tiff or webp, the file will be scaled down without
+#' preserving the aspect ratio.
 #'
 #' @param file_path Path of an existing image file.
 #' @param max_height The maximum height of the image, in pixels. The image will 
@@ -22,52 +25,40 @@ embed_image_cid <- function(
     if (!file.exists(file_path)) {
         stop(paste0(file_path, " is not a valid file path, or does not exist."))
     }
-
-    file_header <- readBin(file_path, "raw", n = 8)
-
-    # Reference headers
-    png_header <- as.raw(strtoi(c("89", "50", "4e", "47", "0d", "0a", "1a", "0a"), 16))
-    jpg_header <- as.raw(strtoi(c("FF", "D8"), 16))
-    bmp_header <- as.raw(strtoi(c("42", "4D"), 16))
-    gif_header <- as.raw(strtoi(c("47", "49", "46"), 16))
     
-    format <- if (identical(file_header, png_header)) {
-        "png"
-    } else if (identical(file_header[1:2], jpg_header)) {
-        "jpg"
-    } else if (identical(file_header[1:2], bmp_header)) {
-        "bmp"
-    } else if (identical(file_header[1:3], gif_header)) {
-        "gif"
-    } else {
-        "unknown"
-    }
-    
-    if (format %in% c("png", "jpg", "bmp")) {
+    if (is.null(image_format(file_path))) {
+        stop(paste0(
+            file_path, 
+            " is not an image (accepted formats: ",
+            "png, jpg, bmp, gif, tiff or webp)"))   
+    } else if (image_format(file_path) %in% c("png", "jpg", "bmp")) {
         image_details <- dim(readbitmap::read.bitmap(file_path))
         original_height <- image_details[1]
         original_width <- image_details[2]
+    
+        # Only reduce, never enlarge
+        # Scale height then width but keep the aspect ratio in tact
+        height_scaling_factor <- min(1, max_height / original_height)   
+        scaled_height <- height_scaling_factor * original_height
+        scaled_width <- height_scaling_factor * original_width
+        width_scaling_factor <- min(1, max_width / scaled_width)
+        scaled_height <- width_scaling_factor * scaled_height
+        scaled_width <- width_scaling_factor * scaled_width    
+        
+        # Can only use an integral number of pixels
+        scaled_height <- floor(scaled_height)
+        scaled_width <- floor(scaled_width)    
+    } else {
+        scaled_height <- max_height
+        scaled_width <- max_width
     }
     
-    # Only reduce, never enlarge
-    # Scale height then width but keep the aspect ratio in tact
-    height_scaling_factor <- min(1, max_height / original_height)   
-    scaled_height <- height_scaling_factor * original_height
-    scaled_width <- height_scaling_factor * original_width
-    width_scaling_factor <- min(1, max_width / scaled_width)
-    scaled_height <- width_scaling_factor * scaled_height
-    scaled_width <- width_scaling_factor * scaled_width    
-    
-    # Can only use an integral number of pixels
-    scaled_height <- floor(scaled_height)
-    scaled_width <- floor(scaled_width)
-    
-    cid_tag <- paste0(
-        "<img src='cid:", basename(file_path),"' ",
-        "width = '", scaled_width, "' ",
-        "height = '", scaled_height, "'",
-        ">"
-    )
+        cid_tag <- paste0(
+            "<img src='cid:", basename(file_path),"' ",
+            "width = '", scaled_width, "' ",
+            "height = '", scaled_height, "'",
+            ">"
+        )
     
     return(cid_tag)
 }
---
+++ R/image_format.R
@@ -0,0 +1,54 @@
+#' Return the image format of an arbitrary file
+#' 
+#' Determines if a given file is a png, jpg, bmp, gif, tiff or webp image,
+#' based on the file signature. If the file is none of these, NULL is returned.
+#' If the file does not exist, an error is thrown.
+#' 
+#' @param file_path File path of a file that may or may not be an image.
+#' @keywords
+
+image_format <- function(file_path) {
+
+    if (!file.exists(file_path)) {
+        stop(paste0(file_path, " is not a valid file path, or does not exist."))
+    }
+    
+    file_header <- readBin(file_path, "raw", n = 12)
+    
+    string_to_raw <- function(string) {
+        string_vector <- unlist(strsplit(string, " "))
+        as.raw(strtoi(string_vector, 16))
+    }
+    
+    # Reference headers
+    png_header <- string_to_raw("89 50 4e 47 0d 0a 1a 0a")
+    jpg_header <- string_to_raw("FF D8 FF")
+    bmp_header <- string_to_raw("42 4D")
+    gif_header <- string_to_raw("47 49 46")
+    tiff_header_little_endian <- string_to_raw("49 49 2A 00")
+    tiff_header_big_endian <- string_to_raw("4D 4D 00 2A")
+    webp_start <- string_to_raw("52 49 46 46")
+    webp_end <- string_to_raw("57 45 42 50") # separated by ?? ?? ?? ??
+    
+    format <- if (identical(file_header[1:8], png_header)) {
+        "png"
+    } else if (identical(file_header[1:3], jpg_header)) {
+        "jpg"
+    } else if (identical(file_header[1:2], bmp_header)) {
+        "bmp"
+    } else if (identical(file_header[1:3], gif_header)) {
+        "gif"
+    } else if (identical(file_header[1:4], tiff_header_little_endian)) {
+        "tiff"
+    } else if (identical(file_header[1:4], tiff_header_big_endian)) {
+        "tiff"
+    } else if (identical(file_header[1:4], webp_start) &
+               identical(file_header[9:12], webp_end)) {
+        "webp"
+    } else {
+        NULL
+    }
+    
+    return(format)
+    
+}
